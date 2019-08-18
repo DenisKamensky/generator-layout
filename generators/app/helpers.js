ec3f77f7a80058b902566cb11b50c8becbbdac9b
DenisKamensky <kengji45@gmail.com>
2019-08-18 23:16:54 +0300

    Added js for gulp.

--- generators/app/helpers.js
+++ generators/app/helpers.js
@@ -36,6 +36,21 @@ module.exports = {
       JSON.devDependencies["gulp-html-prettify"] = "0.0.1";
     }
 
+    if (config.js) {
+      JSON.devDependencies["assets-webpack-plugin"] = "^3.9.10";
+      JSON.devDependencies.gulplog = "^1.0.0";
+      JSON.devDependencies["vinyl-named"] = "^1.1.0";
+      JSON.devDependencies["webpack-stream"] = "^5.2.1";
+      JSON.devDependencies.webpack = "^4.39.2";
+      JSON.devDependencies["@babel/core"] = "^7.5.5";
+      JSON.devDependencies["@babel/preset-env"] = "^7.5.5";
+      JSON.devDependencies["babel-loader"] = "^8.0.6";
+      JSON.devDependencies["@babel/plugin-transform-classes"] = "^7.5.5";
+      JSON.devDependencies["@babel/plugin-transform-destructuring"] = "^7.5.0";
+      JSON.devDependencies["@babel/plugin-transform-template-literals"] =
+        "^7.4.4";
+    }
+
     return JSON;
   }
 };
